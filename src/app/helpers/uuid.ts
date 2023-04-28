98df7a0e0db79c182ff484380680d8f5e39d8d53
John-Paul Simonis <simonis.johnpaul@gmail.com>
2023-04-28 16:09:13 -0700

    name uuid function (lint)

--- src/app/helpers/uuid.ts
+++ src/app/helpers/uuid.ts
@@ -1,2 +1,2 @@
 /** Returns a random unique identifier string. */
-export default function() { return `${Math.floor(Math.random() * 10000)}` }
+export default function uuid() { return `${Math.floor(Math.random() * 10000)}` }
